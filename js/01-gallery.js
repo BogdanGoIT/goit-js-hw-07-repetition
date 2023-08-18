043ab1c39befd563c6658c5b76efeab8acc5f6eb
BogdanGoIT <92575228+BogdanGoIT@users.noreply.github.com>
2023-08-18 09:40:18 +0300

    update filse

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.addEventListener('click', onClick)
+
+function onClick(evt){
+    console.log(evt.target)
+}
+
+
+const gallery = galleryItems.map(({preview, description, original}) => {
+    return(
+        `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+}).join("");
+
+
+galleryEl.insertAdjacentHTML('beforebegin', gallery);
+
+

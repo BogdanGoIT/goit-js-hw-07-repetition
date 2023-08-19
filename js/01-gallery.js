7584cc329650621ec533428cd04df93932cd1899
BogdanGoIT <92575228+BogdanGoIT@users.noreply.github.com>
2023-08-19 23:35:06 +0300

    add files

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,32 +1,77 @@
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
 console.log(galleryItems);
 
 const galleryEl = document.querySelector('.gallery');
+console.log(galleryEl);
 
-galleryEl.addEventListener('click', onClick)
+const galleryItemsEl = galleryItems.map(({original, preview, description}) => (`<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</li>`)
+).join("");
 
-function onClick(evt){
-    console.log(evt.target)
+// console.log(galleryItemsEl);
+
+// galleryEl.insertAdjacentHTML("beforeend", galleryItemsEl);
+
+galleryEl.innerHTML = galleryItemsEl;
+
+
+galleryEl.addEventListener('click', onOpenModal)
+
+function onOpenModal(e){
+  e.preventDefault();
+
+  const imageOriginal = e.target.dataset.source;
+  if(!imageOriginal){
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${imageOriginal}">
+  `)
+
+  instance.show()
+
+  window.addEventListener('keydown', showModal);
+
+  function showModal(e) {
+    if(e.code === "Escape"){
+      instance.close()
+      window.removeEventListener('keydown', showModal)
+    }
+  
+    console.log(e)
+    
+  }
+
+  
+
 }
 
 
-const gallery = galleryItems.map(({preview, description, original}) => {
-    return(
-        `<li class="gallery__item">
-  <a class="gallery__link" href="${original}">
+
+
+
+
+
+
+{/* <li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
     <img
       class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
     />
   </a>
-</li>`
-    )
-}).join("");
-
-
-galleryEl.insertAdjacentHTML('beforebegin', gallery);
-
-
+</li> */}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
 console.log(galleryItems);
+
+const galleryItemsEl = galleryItems.map(({original, description, preview}) => `<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+   <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+</a>
+</li>`).join("");
+
+
+gallery.innerHTML = galleryItemsEl;
+
+var lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250,  });
+
+    
+
+
+{/* <li class="gallery__item">
+   <a class="gallery__link" href="large-image.jpg">
+      <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+   </a>
+</li> */}
